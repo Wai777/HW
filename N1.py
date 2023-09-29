f818993d2f49f6b5d8cf89d225a922c143991552
Wai777 <143869762+Wai777@users.noreply.github.com>
2023-09-30 00:31:19 +0300

    fix N1.py

--- N1.py
+++ N1.py
@@ -1,8 +1,8 @@
-x=int(input())
-y=int(input())
+x=float(input())
+y=float(input())
 if x>y:
     print("1ое больше 2го")
+elif x==y:
+    print("Числа равны")
 else:
-    if(x==y):
-        print("Числа равны")
     print("2ое больше 1го")
