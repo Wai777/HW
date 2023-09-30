2262d9986748162e99e0079a6f70e9bde523f1f1
Wai777 <143869762+Wai777@users.noreply.github.com>
2023-09-30 11:29:32 +0300

    Update N5.py

--- N5.py
+++ N5.py
@@ -1,14 +1,14 @@
-x=float(input())
-y=float(input())
-if x>0 and y>0:
+x = float(input())
+y = float(input())
+if x > 0 and y > 0:
     print('1 четверть')
-if x>0 and y<0:
+elif x > 0 and y < 0:
     print('4 четверть')
-if y==0:
+elif y == 0:
     print('Лежит на оси Оx')
-if x<0 and y>0:
+elif x < 0 and y > 0:
     print('2 четверть')
-if x<0 and y<0:
+elif x < 0 and y < 0:
     print('3 четверть')
-if x==0:
+elif x == 0:
     print('Лежит на оси Оу')
