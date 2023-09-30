e77f1bbef719bceb99958788f7ea29bcd6a8b119
Wai777 <143869762+Wai777@users.noreply.github.com>
2023-09-30 11:33:04 +0300

    Update N2.py

--- N2.py
+++ N2.py
@@ -1,9 +1,9 @@
-a=float(input())
-b=float(input())
-c=float(input())
-d=float(input())
-f=float(input())
-if (f-d)==0:
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+f = float(input())
+if f - d == 0:
     print("На 0 делить нельзя")
 else:
-    print((a*b-c)/(f-d))
+    print((a * b - c) / (f - d))
