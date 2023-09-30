de0a5adb996f0647646f4ea7ba3d87419290513d
Wai777 <143869762+Wai777@users.noreply.github.com>
2023-09-30 11:31:41 +0300

    Update N3.py

--- N3.py
+++ N3.py
@@ -1,6 +1,5 @@
-x=float(input())
-y=float(input())
-z=float(input())
-print(x, y, z)
-r=(x**2+y**2+z**2)**0.5
+x = float(input())
+y = float(input())
+z = float(input())
+r = (x**2+y**2+z**2)**0.5
 print(r)
